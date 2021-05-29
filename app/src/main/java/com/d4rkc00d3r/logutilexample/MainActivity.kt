e9f0abe9971e8d35842adb7f1f701a7ad73b1f5e
Keith <thed4rkc00d3r@gmail.com>
2021-05-29 22:12:02 +0100

    Added example call to example app.

--- app/src/main/java/com/d4rkc00d3r/logutilexample/MainActivity.kt
+++ app/src/main/java/com/d4rkc00d3r/logutilexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.d4rkc00d3r.logutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.d4rkc00d3r.logutil.LogDebug
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogDebug().debugLog(LogDebug().tag, "This is my debug message")
+
     }
 }
